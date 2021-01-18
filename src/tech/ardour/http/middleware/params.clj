9a31101ecc9be795591971975ba138b9a6e3e356
Alexander Scott <xander@axrs.io>
2021-01-18 10:26:45 +1000

    URL Decode params before conversion

--- src/tech/ardour/http/middleware/params.clj
+++ src/tech/ardour/http/middleware/params.clj
@@ -3,13 +3,18 @@
     [cemerick.url :refer [query->map]]
     [clojure.string :as str]
     [tech.ardour.negotiator :as n]
-    [tech.ardour.negotiator.convert :as convert]))
+    [tech.ardour.negotiator.convert :as convert])
+  (:import
+    [java.net URLDecoder]
+    [java.nio.charset Charset]))
+
+(def ^:private ^Charset utf-8-charset (Charset/forName "utf-8"))
 
 (defn wrap-decode [handler]
   (fn [{:keys [query-string] :as request}]
     (handler
       (if (str/blank? query-string)
         request
-        (assoc request :query-params (delay (->> query-string
+        (assoc request :query-params (delay (->> (URLDecoder/decode ^String query-string utf-8-charset)
                                                  (query->map)
                                                  (n/transform-keys string? (comp keyword convert/->kebab-case)))))))))
